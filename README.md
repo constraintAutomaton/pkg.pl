214dcc5e7cbb15dad83be88a89a4767dd69aff30
constraintAutomaton <Vous@exemple.com>
2025-05-01 11:49:31 +0200

    handling of branch and tag dependencies

--- README.md
+++ README.md
@@ -18,7 +18,11 @@ main_file("main.pl").
 % Optional
 dependencies([
     % A git url to clone
-    git("https://github.com/bakaq/testing.pl.git")
+    git("https://github.com/bakaq/testing.pl.git"),
+    % A git url to clone at a specific branch
+    git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", branch("branch")),
+    % A git url to clone at a tag
+    git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", tag("tag"))
 ]).
 ```
 
---
+++ example/main.pl
@@ -0,0 +1,21 @@
+% Loads the package loader
+:- use_module(pkg).
+
+% Loads a package. The argument should be an atom equal to the name of the
+% dependency package specified in the `name/1` field of its manifest.
+:- use_module(pkg(testing)).
+:- use_module(pkg(foo_branch)).
+:- use_module(pkg(foo_tag)).
+
+% You can then use the predicates exported by the main file of the dependency
+% in the rest of the program.
+
+main :-
+    % `run_tests/0` is exported by `pkg(testing)`
+    run_tests,
+    halt.
+
+test("Example success test", (true)).
+test("Example failing test", (false)).
+test("test if the branch dependency works", (tag(exist))).
+test("test if the tag dependency works", (branch(exist))).
