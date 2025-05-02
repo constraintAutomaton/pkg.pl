b7c3e673ca47e94ab5c71301fe933b0de72e1a9d
constraintAutomaton <Vous@exemple.com>
2025-05-02 15:14:13 +0200

    handling of hash dependency.

--- example/main.pl
+++ example/main.pl
@@ -6,6 +6,7 @@
 :- use_module(pkg(testing)).
 :- use_module(pkg(foo_branch)).
 :- use_module(pkg(foo_tag)).
+:- use_module(pkg(foo)).
 
 % You can then use the predicates exported by the main file of the dependency
 % in the rest of the program.
@@ -17,3 +18,5 @@ main :-
 
 test("test if the branch dependency works", (tag(exist))).
 test("test if the tag dependency works", (branch(exist))).
+test("test if the hash dependency works", (hash(exist))).
+
