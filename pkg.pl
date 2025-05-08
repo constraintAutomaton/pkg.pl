8de986814ca2c5e91385ebd5377c1e26455a0df4
constraintAutomaton <Vous@exemple.com>
2025-05-08 21:21:42 +0200

    write lock dependencies to the terminal

--- pkg.pl
+++ pkg.pl
@@ -1,18 +1,24 @@
 /* SPDX-License-Identifier: Unlicense */
 
-:- module(pkg, [pkg_install/0]).
+%:- module(pkg, [pkg_install/0]).
 
 :- use_module(library(os)).
 :- use_module(library(pio)).
 :- use_module(library(files)).
 :- use_module(library(lists)).
 :- use_module(library(format)).
+:- use_module(library(debug)).
 
 scryer_path(ScryerPath) :-
     (   getenv("SCRYER_PATH", ScryerPath) ->
         true
     ;   ScryerPath = "scryer_libs"
     ).
+
+parse_lock_file(Filename, LockFile) :-
+    open(Filename, read, Stream),
+    parse_manifest_(Stream, LockFile),
+    close(Stream).
 
 parse_manifest(Filename, Manifest) :-
     open(Filename, read, Stream),
@@ -49,16 +55,17 @@ pkg_install :-
     setenv("SHELL", "/bin/sh"),
     setenv("GIT_ADVICE", "0"),
     (   member(dependencies(Deps), Manifest) ->
-        ensure_dependencies(Deps)
+        ensure_dependencies(Deps),
+        write(Ls)
     ;   true
     ).
 
-ensure_dependencies([]).
+ensure_dependencies([], []).
 ensure_dependencies([D|Ds]) :-
     ensure_dependency(D),
-    ensure_dependencies(Ds).
+    ensure_dependencies(Ds).    
 
-ensure_dependency(DependencyTerm) :-
+ensure_dependency(DependencyTerm, LockDependencyTerm) :-
     current_output(Out),
     phrase_to_stream(("Ensuring is installed: ", portray_clause_(DependencyTerm)), Out),
     shell("rm --recursive --force scryer_libs/tmp"),
@@ -74,7 +81,10 @@ ensure_dependency(DependencyTerm) :-
         ],
         Command2
     ),
-    shell(Command2).
+    shell(Command2),
+    append(["scryer_libs/", Name], Path),
+    lock_dependency(DependencyTerm, Path, LockDependencyTerm),
+    write(LockDependencyTerm).
 
 dependency_aq_command(git(X), Command) :- git_command(git(X), Command).
 dependency_aq_command(git(X, Y), Command) :- git_command(git(X, Y), Command).
@@ -97,10 +107,36 @@ git_command(git(Url, tag(Tag)), Command) :-
 
 git_command(git(Url, hash(Hash)), Command) :-
     CloneCommand = ["git clone --quiet --depth 1 --single-branch ", Url, " scryer_libs/tmp "],
-    GetHashCommitCommand = [" && cd scryer_libs/tmp  >/dev/null && git fetch --quiet --depth 1 origin ", Hash, " && git checkout --quiet ", Hash, " && cd ../  >/dev/null"],
+    GetHashCommitCommand = [" && cd scryer_libs/tmp  >/dev/null && git fetch --quiet --depth 1 origin ", Hash, " && git checkout --quiet ", Hash],
     append(CloneCommand, GetHashCommitCommand,  Segments), 
     append(Segments, Command).
 
 path_command(path(Path), Command) :-
     Segments = ["ln -rs ", Path, " scryer_libs/tmp"],
     append(Segments, Command).
+
+lock_dependency(git(Url), Path, LockDependencyTerm) :-
+    append(["cd ", Path, " && git rev-parse HEAD"], Command),
+    run_command(Command, temp_result(Hash)),
+    LockDependencyTerm=git(Url, hash(Hash)).
+
+lock_dependency(git(Url, tag(_)), Path, LockDependencyTerm) :-
+    lock_dependency(git(Url), Path, LockDependencyTerm).
+
+lock_dependency(git(Url, branch(_)), Path, LockDependencyTerm) :-
+    lock_dependency(git(Url), Path, LockDependencyTerm).
+
+lock_dependency(git(Url, hash(Hash)), _, git(Url, hash(Hash))).
+
+lock_dependency(path(Path), _, path(Path)).
+
+/**
+ A hack to get the result of a shell command.
+*/
+run_command(Command, Output) :-
+    append(["echo \"temp_result(\\\"$(",Command, ")\\\").\" > temp"], Command2),
+    shell(Command2),
+    open('temp', read, Stream),
+    read(Stream, Output),
+    close(Stream),
+    shell("rm temp").
