f9fc6150bc0568eafb5d97eb6dccf9512ba84223
bakaq <bakaq@bakaq.net>
2025-05-14 14:15:37 -0300

    Use unpacked scripts to do the complicated steps

--- example/scryer-manifest.pl
+++ example/scryer-manifest.pl
@@ -4,13 +4,13 @@ main_file("main.pl").
 % Optional
 dependencies([
     % A git url to clone
-    git("https://github.com/bakaq/testing.pl.git"),
+    dependency("testing", git("https://github.com/bakaq/testing.pl.git")),
     % A git url to clone at a specific branch
-    git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", branch("branch")),
+    dependency("test_branch", git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", branch("branch"))),
     % A git url to clone at a tag
-    git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", tag("tag")),
+    dependency("test_tag", git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", tag("tag"))),
     % A git url to clone at a specific commit hash
-    git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", hash("d19fefc1d7907f6675e181601bb9b8b94561b441")),
+    dependency("test_hash", git("https://github.com/constraintAutomaton/test-prolog-package-manager.git", hash("d19fefc1d7907f6675e181601bb9b8b94561b441"))),
     % A path to a local package
-    path("./local_package")
+    dependency("test_local", path("./local_package"))
 ]).
