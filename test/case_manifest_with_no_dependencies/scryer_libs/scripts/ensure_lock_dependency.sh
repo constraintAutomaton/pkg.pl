468de903b3b463550d2a55c719cacd409afa7e85
constraintAutomaton <Vous@exemple.com>
2025-05-19 00:09:46 +0200

    ignore files added

--- test/case_manifest_with_no_dependencies/scryer_libs/scripts/ensure_lock_dependency.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-GIT_HASH=$(cd scryer_libs/packages/${DEPENDENCY_NAME} && git rev-parse HEAD)
-echo "result(\"$GIT_HASH\")." > ${RESULT_FILE}
--- test/case_manifest_with_no_dependency_predicate/scryer_libs/scripts/ensure_integrity_hash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-CHECKSUM=$(find scryer_libs/packages/${DEPENDENCY_NAME} -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')
-echo "result(\"$CHECKSUM\")." > ${RESULT_FILE}
