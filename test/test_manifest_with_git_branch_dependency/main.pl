0573a84a966786ac7fe17693abe5f21b175cc1cf
constraintAutomaton <Vous@exemple.com>
2025-05-24 20:27:55 +0200

    made test run with justfiles

--- test/test_manifest_with_git_branch_dependency/main.pl
+++ test/test_manifest_with_git_branch_dependency/main.pl
@@ -1,18 +1,9 @@
 :- use_module(pkg).
 :- use_module('../util_packages/scryer_libs/packages/testing/testing.pl').
-:- use_module(library(os)).
-:- use_module(library(pio)).
 
 main :-
-    shell("rm -rf scryer_libs && rm -f manifest-lock.pl"),
-    (
-        run_tests([halt(false)])->
-            Code=0
-        ;   Code=1
-    ),
-    shell("rm -rf scryer_libs && rm -f manifest-lock.pl"), 
-    halt(Code).
-
+    run_tests.
+  
 test("the package report is valid", (
         pkg_install(X),
         X = [
---
+++ test/test_manifest_with_no_dependency_predicate/main.pl
@@ -0,0 +1,7 @@
+:- use_module(pkg).
+:- use_module('../util_packages/scryer_libs/packages/testing/testing.pl').
+
+main :-
+    run_tests.
+
+test("test if no dependencies are installed", (pkg_install(X), X == [])).
