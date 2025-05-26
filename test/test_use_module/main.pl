3ea0916f31813e64a30946ae7bdefa564774f825
constraintAutomaton <Vous@exemple.com>
2025-05-26 18:16:10 +0200

    test of use_module

---
+++ test/test_use_module/main.pl
@@ -0,0 +1,16 @@
+:- use_module(pkg).
+:- use_module(pkg(test)).
+:- use_module(pkg(test_branch)).
+:- use_module(pkg(test_tag)).
+:- use_module(pkg(test_hash)).
+:- use_module(pkg(test_local)).
+:- use_module('../util_packages/scryer_libs/packages/testing/testing.pl').
+
+main :-
+    run_tests.
+
+test("test if the git dependency work", (main_code(exist))).
+test("test if the branch dependency works", (tag(exist))).
+test("test if the tag dependency works", (branch(exist))).
+test("test if the hash dependency works", (hash(exist))).
+test("test if the local dependency works", (local(exist))).
