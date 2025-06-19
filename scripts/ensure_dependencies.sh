2619a1a280bc6bca3532e7547dfdfa5e7e64210a
constraintAutomaton <Vous@exemple.com>
2025-06-19 21:40:16 +0200

    progression

--- scripts/ensure_dependencies.sh
+++ scripts/ensure_dependencies.sh
@@ -2,24 +2,48 @@
 set -u
 
 write_result() {
-    flock scryer_libs/temp/install_resp.pl.lock -c \
-        "printf 'result(\"%s\", %s).\n' \"$1\" \"$2\" >> scryer_libs/temp/install_resp.pl"
+    (
+        flock 200
+        printf 'result("%s", %s, %s).\n' "$1" "$2" "$3" >> scryer_libs/temp/install_resp.pl
+    ) 200>scryer_libs/temp/install_resp.pl.lock
 }
 
 write_success() {
-    write_result "$1" "success"
+    write_result "$1" "success" "$2"
 }
 
 write_error() {
     escaped_error=$(printf '%s' "$2" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g')
     escaped_error=$(printf '%s' "$escaped_error" | tr '\r\n' '\\n')
     escaped_error=$(printf '%s' "$escaped_error" | sed 's/ / /g')
-    write_result "$1" "error(\\\"$escaped_error\\\")"
+    write_result "$1" "error(\\\"$escaped_error\\\")" "$3"
+}
+
+lock_dependency() {
+    dependency_name=$1
+
+    ERROR=""
+
+    cd "scryer_libs/packages/${dependency_name}"
+
+    GIT_HASH=$(git rev-parse HEAD 2>&1) || ERROR="$ERROR git failure: $GIT_HASH"
+    GIT_HASH=$(echo "$GIT_HASH" | head -n1)
+
+    if [ -z "$ERROR" ]; then
+        printf 'lock("%s")' "$GIT_HASH"
+    else
+        escaped_error=$(printf '%s' "$ERROR" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g')
+        escaped_error=$(printf '%s' "$escaped_error" | tr '\r\n' '\\n')
+        escaped_error=$(printf '%s' "$escaped_error" | sed 's/ / /g')
+
+        printf "error(\"%s\")" "$escaped_error"
+    fi
 }
 
 install_git_default() {
     dependency_name=$1
     git_url=$2
+    lock=$3
 
     error_output=$(
         git clone \
@@ -31,7 +55,11 @@ install_git_default() {
     )
 
     if [ -z "$error_output" ]; then
-        write_success "${dependency_name}"
+        LOCK_TERM="nothing"
+        if [ "$lock" = "true" ]; then
+            LOCK_TERM=$(lock_dependency "${dependency_name}")
+        fi
+        write_success "${dependency_name}" "$LOCK_TERM"
     else
         write_error "${dependency_name}" "$error_output"
     fi
@@ -41,6 +69,7 @@ install_git_branch() {
     dependency_name=$1
     git_url=$2
     git_branch=$3
+    lock=$4
 
     error_output=$(
         git clone \
@@ -53,7 +82,11 @@ install_git_branch() {
     )
 
     if [ -z "$error_output" ]; then
-        write_success "${dependency_name}"
+        LOCK_TERM="nothing"
+        if [ "$lock" = "true" ]; then
+            LOCK_TERM=$(lock_dependency "${dependency_name}")
+        fi
+        write_success "${dependency_name}" "${LOCK_TERM}"
     else
         write_error "${dependency_name}" "$error_output"
     fi
@@ -63,6 +96,7 @@ install_git_tag() {
     dependency_name=$1
     git_url=$2
     git_tag=$3
+    lock=$4
 
     error_output=$(
         git clone \
@@ -75,7 +109,11 @@ install_git_tag() {
     )
 
     if [ -z "$error_output" ]; then
-        write_success "${dependency_name}"
+        LOCK_TERM="nothing"
+        if [ "$lock" = "true" ]; then
+            LOCK_TERM=$(lock_dependency "${dependency_name}")
+        fi
+        write_success "${dependency_name}" "${LOCK_TERM}"
     else
         write_error "${dependency_name}" "$error_output"
     fi
@@ -85,6 +123,7 @@ install_git_hash() {
     dependency_name=$1
     git_url=$2
     git_hash=$3
+    lock=$4
 
     error_output=$(
         git clone \
@@ -111,7 +150,11 @@ install_git_hash() {
         combined_error="${fetch_error}; ${switch_error}"
 
         if [ -z "$fetch_error" ] && [ -z "$switch_error" ]; then
-            write_success "${dependency_name}"
+            LOCK_TERM="nothing"
+            if [ "$lock" = "true" ]; then
+                LOCK_TERM=$(lock_dependency "${dependency_name}")
+            fi
+            write_success "${dependency_name}" "${LOCK_TERM}"
         else
             write_error "${dependency_name}" "$combined_error"
         fi
@@ -128,7 +171,8 @@ install_path() {
         error_output=$(ln -rsf "${dependency_path}" "scryer_libs/packages/${dependency_name}" 2>&1 1>/dev/null)
 
         if [ -z "$error_output" ]; then
-            write_success "${dependency_name}"
+            LOCK_TERM="nothing"
+            write_success "${dependency_name}" "${LOCK_TERM}"
         else
             write_error "${dependency_name}" "$error_output"
         fi
@@ -145,7 +189,7 @@ IFS=$OLD_IFS
 touch scryer_libs/temp/install_resp.pl
 
 for dependency in "$@"; do
-    unset dependency_term dependency_kind dependency_name git_url git_branch git_tag git_hash dependency_path
+    unset dependency_term dependency_kind dependency_name git_url git_branch git_tag git_hash dependency_path lock
 
     IFS=';'
     set -- $dependency
@@ -167,6 +211,7 @@ for dependency in "$@"; do
         git_tag) git_tag=$value ;;
         git_hash) git_hash=$value ;;
         dependency_path) dependency_path=$value ;;
+        lock) lock=$value ;;
         esac
     done
 
@@ -176,16 +221,16 @@ for dependency in "$@"; do
     do_nothing) ;;
 
     git_default)
-        install_git_default "${dependency_name}" "${git_url}" &
+        install_git_default "${dependency_name}" "${git_url}" "${lock}" &
         ;;
     git_branch)
-        install_git_branch "${dependency_name}" "${git_url}" "${git_branch}" &
+        install_git_branch "${dependency_name}" "${git_url}" "${git_branch}" "${lock}" &
         ;;
     git_tag)
-        install_git_tag "${dependency_name}" "${git_url}" "${git_tag}" &
+        install_git_tag "${dependency_name}" "${git_url}" "${git_tag}" "${lock}" &
         ;;
     git_hash)
-        install_git_hash "${dependency_name}" "${git_url}" "${git_hash}" &
+        install_git_hash "${dependency_name}" "${git_url}" "${git_hash}" "${lock}" &
         ;;
     path)
         install_path "${dependency_name}" "${dependency_path}" &
