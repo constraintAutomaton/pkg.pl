07aff16f6943033bb7ddee106cfdc15aa48d83ed
bakaq <bakaq@bakaq.net>
2025-07-13 05:54:08 -0300

    Add some snapshot tests

---
+++ tests/snapshot/cases/cli_help.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+bakage() {
+    ../../bakage.pl "$@"
+}
+
+echo "=== Testing help with various different color configurations ==="
+bakage --help --color always
+echo "================================================================"
+bakage --help --color never
+echo "================================================================"
+CLICOLOR_FORCE=1 bakage --help
+echo "================================================================"
+NO_COLOR=1 bakage --help
+
+echo "=== Install help ==="
+bakage install --help --color always
+echo "================================================================"
+bakage install --help --color never
+echo "================================================================"
