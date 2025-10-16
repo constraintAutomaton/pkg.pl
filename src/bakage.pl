05b41c1b66ae804d3afa15809f951b2cded624a3
bakaq <bakaq@bakaq.net>
2025-10-16 14:55:04 -0300

    PR #54 nits and cleanup

--- src/bakage.pl
+++ src/bakage.pl
@@ -1,24 +1,6 @@
-/*/bin/sh -c true
-
-set -eu
-
-type scryer-prolog > /dev/null 2> /dev/null \
-    && exec scryer-prolog -f -g "bakage:run" "$0" -- "$@"
-
-echo "No known supported Prolog implementation available in PATH."
-echo "Try to install Scryer Prolog."
-exit 1
-#*/
-
 /* SPDX-License-Identifier: Unlicense */
 
 :- module(bakage, [pkg_install/1]).
-
-% === Dev import start ===
-:- use_module('cli.pl').
-:- use_module('scripts.pl').
-% === Dev import end ===
-
 
 % ==========================
 % === Compatibility zone ===
